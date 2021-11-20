fbe2bb8dff31a5840c2feb641feffd3f0ec4ae9b
Jaap Eldering <jaap@jaapeldering.nl>
2021-11-20 12:45:55 +0100

    Move doc building code to gh-pages branch.

--- update_docs.sh
+++ update_docs.sh
@@ -5,16 +5,16 @@ TMPDIR=$(mktemp -d -t 'gen-gh-pages-XXXXXX')
 REPO_DIR="$TMPDIR/ccs-specs"
 MY_DIR=$(realpath $(dirname $0))
 
-git clone "$MY_DIR" "$REPO_DIR"
+mkdir -p "$REPO_DIR"
+rsync -a "$MY_DIR/" "$REPO_DIR/"
 
 for version in $(cat versions.json | jq -r -c '.[]'); do
-	rm -rf "$version/"
-	mkdir -p "$version"
-	(
-		cd "$REPO_DIR"
-		git checkout -b "${version}" -t "origin/${version}"
-		bundle exec jekyll build -d "$MY_DIR/${version}/"
-	)
+    rm -rf "$version/"
+    mkdir -p "$version"
+    ( cd "$REPO_DIR" && git checkout "${version}" )
+	ln -sf "$MY_DIR/_layouts" "$REPO_DIR"
+    bundle exec jekyll build --config _config.yml -b "/${version}" \
+        -s "$REPO_DIR" -d "${version}/"
 done
 
 rm -rf "$TMPDIR"
