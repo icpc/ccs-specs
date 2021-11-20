326fe4bf0f8cb3bc32fe1c6e459da4adba3d147f
Jaap Eldering <jaap@jaapeldering.nl>
2021-11-20 16:44:01 +0100

    Add general readme to this branch.

--- update_docs.sh
+++ update_docs.sh
@@ -8,13 +8,16 @@ MY_DIR=$(realpath $(dirname $0))
 mkdir -p "$REPO_DIR"
 rsync -a "$MY_DIR/" "$REPO_DIR/"
 
+rm -rf _site/
+
+bundle exec jekyll build
+
 for version in $(cat versions.json | jq -r -c '.[]'); do
-    rm -rf "$version/"
-    mkdir -p "$version"
+    mkdir -p "_site/$version"
     ( cd "$REPO_DIR" && git checkout "${version}" )
 	ln -sf "$MY_DIR/_layouts" "$REPO_DIR"
     bundle exec jekyll build --config _config.yml -b "/${version}" \
-        -s "$REPO_DIR" -d "${version}/"
+        -s "$REPO_DIR" -d "_site/${version}/"
 done
 
 rm -rf "$TMPDIR"
