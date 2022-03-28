768239f9ab4fd4d3cc7b9a9cee7027b549f90070
Jaap Eldering <jaap@jaapeldering.nl>
2022-03-28 22:46:07 +0200

    Automatically commit documentation updates.

--- update_docs.sh
+++ update_docs.sh
@@ -12,12 +12,23 @@ rm -rf docs
 
 bundle exec jekyll build -d docs/
 
+commits=''
 for version in $(cat versions.json | jq -r -c '.[]'); do
-    ( cd "$REPO_DIR" && git checkout "${version}" )
+	cd "$REPO_DIR"
+	git checkout "${version}"
+	commitsha=$(git rev-parse --short=10 HEAD)
+	commits="${commits}
+- ${version} generated from ${commitsha}"
+	cd -
+
 	ln -sf "$MY_DIR/_layouts" "$REPO_DIR"
 	echo "version: ${version}" > "$TMPDIR/version.yml"
-    bundle exec jekyll build --config _config.yml,"$TMPDIR/version.yml" \
-        -b "/${version}" -s "$REPO_DIR" -d "docs/${version}/"
+	bundle exec jekyll build --config _config.yml,"$TMPDIR/version.yml" \
+		-b "/${version}" -s "$REPO_DIR" -d "docs/${version}/"
 done
+
+git add --all
+git commit -a -m "Automatically rebuild docs:
+${commits}"
 
 rm -rf "$TMPDIR"
